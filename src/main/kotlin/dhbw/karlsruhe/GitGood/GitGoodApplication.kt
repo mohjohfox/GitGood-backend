61f62c6404323ac669beb423a25f22d61b15d2d5
mohjohfox <marcorobinf@gmail.com>
2021-11-18 12:30:34 +0100

    init commit

---
+++ src/main/kotlin/dhbw/karlsruhe/GitGood/GitGoodApplication.kt
@@ -0,0 +1,11 @@
+package dhbw.karlsruhe.GitGood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitGoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<GitGoodApplication>(*args)
+}
---
+++ src/test/kotlin/dhbw/karlsruhe/GitGood/GitGoodApplicationTests.kt
@@ -0,0 +1,13 @@
+package dhbw.karlsruhe.GitGood
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitGoodApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
