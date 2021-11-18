dbab6d7f77d14c9badd0a2dbc4f76574c8797456
mohjohfox <marcorobinf@gmail.com>
2021-11-18 15:46:52 +0100

    open endpoints

--- src/main/kotlin/dhbw/karlsruhe/gitgood/security/Security.kt
+++ src/main/kotlin/dhbw/karlsruhe/gitgood/security/Security.kt
@@ -1,37 +1,39 @@
 package dhbw.karlsruhe.gitgood.security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class Security : WebSecurityConfigurerAdapter() {
 
-    @Throws(Exception::class)
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        val encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
-        auth
-            .inMemoryAuthentication()
-            .withUser("user")
-            .password(encoder.encode("password"))
-            .roles("USER")
-            .and()
-            .withUser("admin")
-            .password(encoder.encode("admin"))
-            .roles("USER", "ADMIN")
-    }
-
+    /*
+     * Definiert, welche URL Pfade gesichert werden sollen und welche nicht
+     */
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic()
+        http.authorizeRequests().antMatchers("/test", "/").permitAll().and().csrf()
+            .ignoringAntMatchers("/login")
+        http.exceptionHandling().accessDeniedPage("/access-denied")
+        http.cors()
+    }
+
+    // To enable CORS
+    @Bean
+    open fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOriginPatterns = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowCredentials = true
+        configuration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
