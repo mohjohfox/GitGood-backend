043d0c6fb7f4a11a515ef4d3ef6ce6d002d8d08c
mohjohfox <marcorobinf@gmail.com>
2022-05-06 14:05:16 +0200

    GG-106: refactored HundredOneService into smaller submethods

--- src/main/java/dhbw/karlsruhe/gitgood/service/CalculationService.java
+++ src/main/java/dhbw/karlsruhe/gitgood/service/CalculationService.java
@@ -9,14 +9,12 @@ public abstract class CalculationService {
 
   abstract Game calculateRound(Game game);
 
+  abstract boolean isFinished(Player player);
+
   protected void selectNextPlayer(Game game){
     List<Player> players = game.getPlayers();
     int i = players.indexOf(game.getCurrentPlayer());
     game.setCurrentPlayer(players.get((i + 1) % players.size()));
-  }
-
-  protected boolean isFinished(Player player) {
-    return player.getPoints() == 0;
   }
 
 }
--- src/main/java/dhbw/karlsruhe/gitgood/service/HundredOneService.java
+++ src/main/java/dhbw/karlsruhe/gitgood/service/HundredOneService.java
@@ -11,18 +11,26 @@ public class HundredOneService extends CalculationService {
   @Override
   public Game calculateRound(Game game){
     Player currentPlayer = game.getCurrentPlayer();
-    int sumOfThrownPoints = findSumUsingStream(currentPlayer.getThrownPoints());
-    int oldPoints = currentPlayer.getPoints();
-    int newPoints = oldPoints - sumOfThrownPoints;
 
-    if (newPoints >= 0){
-      currentPlayer.setPoints(newPoints);
-    }
+    calculateNewPointsForCurrPlayer(currentPlayer, findSumUsingStream(currentPlayer.getThrownPoints()));
 
     game.setFinished(isFinished(currentPlayer));
+
     selectNextPlayer(game);
 
     return game;
+  }
+
+  private void calculateNewPointsForCurrPlayer(Player currPlayer, int sumOfThrownPoints){
+    int newPoints = currPlayer.getPoints() - sumOfThrownPoints;
+
+    if (newPoints >= 0){
+      currPlayer.setPoints(newPoints);
+    }
+  }
+
+  protected boolean isFinished(Player player) {
+    return player.getPoints() == 0;
   }
 
   private int findSumUsingStream(String[] array) {
@@ -30,6 +38,5 @@ public class HundredOneService extends CalculationService {
         .mapToInt(Integer::intValue)
         .sum();
   }
-
 
 }
