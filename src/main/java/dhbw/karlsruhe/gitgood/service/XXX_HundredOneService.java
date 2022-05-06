b52554aa1054c32877d919d1fb18a1f5ee68e8a7
mohjohfox <marcorobinf@gmail.com>
2022-05-06 11:35:38 +0200

    GG-106: renamed service class

--- src/main/java/dhbw/karlsruhe/gitgood/service/XXX_HundredOneService.java
+++ src/main/java/dhbw/karlsruhe/gitgood/service/XXX_HundredOneService.java
@@ -3,13 +3,10 @@ package dhbw.karlsruhe.gitgood.service;
 import dhbw.karlsruhe.gitgood.model.Game;
 import dhbw.karlsruhe.gitgood.model.Player;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ThreeHundredOneService extends CalculationService {
+public class XXX_HundredOneService extends CalculationService {
 
   @Override
   public Game calculateRound(Game game){
