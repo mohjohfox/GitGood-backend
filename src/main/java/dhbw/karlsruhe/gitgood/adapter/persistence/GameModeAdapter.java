d9b27c56757ebe7416cb82c8f5cdb040a385852e
mohjohfox <marcorobinf@gmail.com>
2022-07-09 20:09:56 +0200

    Query database for all gamemodes - deleted GameModeEnum

--- src/main/java/dhbw/karlsruhe/gitgood/adapter/persistence/GameModeAdapter.java
+++ src/main/java/dhbw/karlsruhe/gitgood/adapter/persistence/GameModeAdapter.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -29,14 +30,23 @@ public class GameModeAdapter implements GameModePort {
 
 
         return maps.isEmpty() ? Optional.empty() : Optional.of(new GameMode(
-                maps.get(0).get("name").toString(),
+                maps.get(0).get("shortName").toString(),
                 maps.get(0).get("description").toString()));
     }
 
     @Override
     public List<GameMode> getAllGameModes() {
+
+        List<GameMode> allGameModes = new ArrayList<>();
+
         List<Map<String, Object>> maps = jdbc.queryForList("SELECT * FROM gamemode");
 
+        if (!maps.isEmpty()){
+            maps.forEach( item -> {
+                allGameModes.add(new GameMode( item.get("shortName").toString(), item.get("description").toString() ));
+            });
+            return allGameModes;
+        }
         return List.of();
     }
 }
